b26aced171558044a1ab6a95aba78c6e1a362e67
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-07-14 21:53:47 +0700

    feat: add Input component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Input from "./components/Input/Input";
 
-function App() {
+export default function App() {
+  
+  const [currentPassword, setCurrentPassword] = useState("");
+
+  function handlePasswordChange(newPassword) {
+    setCurrentPassword(newPassword);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Input value={currentPassword} onChange={handlePasswordChange}/>
   );
 }
-
-export default App;
